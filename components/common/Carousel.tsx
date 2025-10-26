5f1e92169fbf7cbbcbd75a00642598ebdf98d415
Utkarsh Tiwari <tiwariutkarsh007@gmail.com>
2025-10-26 14:04:15 +0530

    Add preview in carousel

--- components/common/Carousel.tsx
+++ components/common/Carousel.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import React, { useState } from 'react';
+
 import Image from 'next/image';
+import Modal from './Modal';
 
 type CarouselProps = {
   images: any;
@@ -9,6 +11,7 @@ type CarouselProps = {
 
 const Carousel = ({ images }: CarouselProps) => {
   const [activeImageIndex, setActiveImageIndex] = useState(0);
+  const [isPreviewOpen, setIsPreviewOpen] = useState(false);
 
   const navigateImages = (type: string) => {
     let newActiveImageIndex;
@@ -24,47 +27,77 @@ const Carousel = ({ images }: CarouselProps) => {
     setActiveImageIndex(newActiveImageIndex);
   };
 
-  return (
-    <div className='carousel'>
-      <div className='carousel__container'>
-        {images.map(({ id, image, caption }: any, index: number) => {
-          if (activeImageIndex === index) {
-            return (
-              <div key={id} className='carousel__slide fade'>
-                <div className='carousel__number'>
-                  {++index} / {images.length}
+  const renderCarousel = () => {
+    return (
+      <div className='carousel'>
+        <div className='carousel__container'>
+          {images.map(({ id, image, caption }: any, index: number) => {
+            if (activeImageIndex === index) {
+              return (
+                <div
+                  key={id}
+                  className='carousel__slide fade'
+                  onClick={() => !isPreviewOpen && setIsPreviewOpen(true)}
+                >
+                  <div
+                    className={`carousel__number ${isPreviewOpen ? 'carousel__number--preview' : ''}`}
+                  >
+                    {++index} / {images.length}
+                  </div>
+                  <Image
+                    className={`carousel__image ${isPreviewOpen ? 'carousel__image--preview' : ''}`}
+                    src={image}
+                    alt={caption}
+                  />
+                  <div
+                    className={`carousel__caption ${isPreviewOpen ? 'carousel__caption--preview' : ''}`}
+                  >
+                    {caption}
+                  </div>
                 </div>
-                <Image className='carousel__image' src={image} alt={caption} />
-                <div className='carousel__caption'>{caption}</div>
-              </div>
-            );
-          }
-        })}
+              );
+            }
+          })}
 
-        <a
-          className='carousel__control carousel__control--prev'
-          onClick={() => navigateImages('prev')}
-        >
-          &#10094;
-        </a>
-        <a
-          className='carousel__control carousel__control--next'
-          onClick={() => navigateImages('next')}
-        >
-          &#10095;
-        </a>
-      </div>
+          <a
+            className={`carousel__control carousel__control--prev ${isPreviewOpen ? 'carousel__control--preview' : ''}`}
+            onClick={() => navigateImages('prev')}
+          >
+            &#10094;
+          </a>
+          <a
+            className={`carousel__control carousel__control--next ${isPreviewOpen ? 'carousel__control--preview' : ''}`}
+            onClick={() => navigateImages('next')}
+          >
+            &#10095;
+          </a>
+        </div>
 
-      <div className='carousel__dots'>
-        {images.map((_item: any, index: number) => (
-          <span
-            key={index}
-            className={`carousel__dot ${activeImageIndex === index ? 'carousel__dot--active' : ''}`}
-            onClick={() => setActiveImageIndex(index)}
-          ></span>
-        ))}
+        <div className='carousel__dots'>
+          {images.map((_item: any, index: number) => (
+            <span
+              key={index}
+              className={`carousel__dot ${activeImageIndex === index ? 'carousel__dot--active' : ''}`}
+              onClick={() => setActiveImageIndex(index)}
+            ></span>
+          ))}
+        </div>
       </div>
-    </div>
+    );
+  };
+
+  return (
+    <>
+      {renderCarousel()}
+      {isPreviewOpen && (
+        <Modal
+          className='carousel__preview-modal'
+          onClose={() => setIsPreviewOpen(false)}
+        >
+          {renderCarousel()}
+        </Modal>
+      )}
+    </>
   );
 };
 
